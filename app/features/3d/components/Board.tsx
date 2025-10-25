2e6d5bac2adf44442d60d6a2bb512ea2efe0bfaa
imaphatduc <vutranducphat@gmail.com>
2025-10-25 21:39:47 +0700

    feat: change icon pack

--- app/features/3d/components/Board.tsx
+++ app/features/3d/components/Board.tsx
@@ -1,4 +1,5 @@
 import { GeoFeature } from "@/app/types";
+import { MessageCircle, Ruler, Star, Users } from "lucide-react";
 
 interface Props {
   selectedCountry: GeoFeature;
@@ -25,19 +26,25 @@ const Board = ({ selectedCountry }: Props) => {
         </h1>
       </div>
       <div className="grid grid-cols-[1.5rem,1fr] gap-x-3 gap-y-5 items-center">
-        <div className="text-xl self-start">🌟</div>
+        <div className="text-xl self-start">
+          <Star />
+        </div>
         <div className="space-y-2">
           {CAPITALS.map((capital, i) => (
             <p key={i}>{capital}</p>
           ))}
         </div>
-        <div className="text-xl self-start">🗪</div>
+        <div className="text-xl self-start">
+          <MessageCircle />
+        </div>
         <div className="space-y-2">
           {OFFICIAL_LANGUAGES.map((language, i) => (
             <p key={i}>{language[0].toUpperCase() + language.slice(1)}</p>
           ))}
         </div>
-        <div className="text-sm">👨‍👩‍👧‍👦</div>
+        <div className="text-sm">
+          <Users />
+        </div>
         <div>
           {POPULATION && (
             <p>
@@ -46,7 +53,9 @@ const Board = ({ selectedCountry }: Props) => {
             </p>
           )}
         </div>
-        <div className="text-xl">📐</div>
+        <div className="text-xl">
+          <Ruler />
+        </div>
         <div>
           {AREA && <p>{new Intl.NumberFormat().format(AREA) + " km²"}</p>}
         </div>
